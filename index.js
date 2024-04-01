28e3f897aae120857253f06513abf57ced46f7af
Anni Horko <annihorko92@gmail.com>
2024-04-01 14:26:22 +0300

    added 404

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+// Dummy database. IRL we could get the data from the MongoDB.
+const products =
+[
+    {
+        name: 'Kia',
+        price: 25000
+    },
+    {
+        name: 'Volvo',
+        price: 30000
+    }
+]
+
 app.engine('handlebars',exphbs.engine({
     defaultLayout: 'main'
 }));
@@ -29,8 +42,21 @@ app.get('/contact', (req,res) => {
     );
 });
 
+app.get('/products', (req,res) =>{
+    res.render('products',
+    {
+        title: 'Products',
+        products: products
+    }
+    )
+});
+
 //Folder for static files like css, jpg...
 app.use(express.static('public'));
+
+app.use((req,res,next) => {
+    res.status(404).send("Sorry, could not find the content");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
